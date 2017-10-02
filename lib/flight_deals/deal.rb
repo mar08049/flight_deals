6ce384e4f121c4167632416d66d2287c7ea46802
Cole Martin <mar08049@gmail.com>
2017-10-02 16:29:40 +0000

    scraping

--- lib/flight_deals/deal.rb
+++ lib/flight_deals/deal.rb
@@ -7,15 +7,11 @@ class FlightDeals::Deal
 
   def self.scrape_url
     doc = Nokogiri::HTML(open("https://www.theflightdeal.com/"))
-
+    binding.pry
     deal = self.new
-<<<<<<< HEAD
     deal.name = doc.search("h1.post-title cufon_headings").text.strip.scan(/.*\./)
-    
-=======
-    deal.name = doc.search("h2.post-title cufon_headings").text.scan(/.*\./)
     deal.desc = doc.search("p").text
->>>>>>> a67fde1d111515763155e27b60af1a318cd5afae
+  
     deal
   end
   def self.scrape_deals
