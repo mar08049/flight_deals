c0bad9a59e05e9c0f936bda066e1ef3f0f8020af
Cole <mar08049@gmail.com>
2017-10-05 11:24:02 -0500

    Refactoring during assessment.

--- lib/flight_deals/deal.rb
+++ lib/flight_deals/deal.rb
@@ -1,22 +1,13 @@
+require 'pry'
 class FlightDeals::Deal
   attr_accessor :name, :desc
 
-  def self.today
-    self.scrape_deals
+  @@all = []
+  def initialize
+    @@all << self
   end
 
-  def self.scrape_url
-
-    doc = Nokogiri::HTML(open("https://escapehouston.com/category/cheap-flights-from-houston/"))
-    deal = self.new
-    deal.name = doc.css("h2.excerpt-title a").text.scan(/^[^$]+/)
-    deal.desc = doc.css(".excerpt-content p").text.scan(/^[^.]+/)
-    deal
+  def self.all
+    @@all
   end
-
-  def self.scrape_deals
-   deals = []
-   deals << self.scrape_url
-   deals
- end
 end
